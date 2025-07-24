b786389217592e566567d57816fa62a88f62a55d
charu <charupsharma5@gmail.com>
2025-07-25 00:39:47 +0530

    add google auth API integration

--- frontend/src/components/GoogleLogin.jsx
+++ frontend/src/components/GoogleLogin.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useGoogleLogin } from "@react-oauth/google";
+import { googleAuth } from "../api";
 
 const GoogleLogin = () => {
   const googleResponse = async (authResult) => {
     try {
       console.log(authResult);
+      if (authResult["code"]) {
+        const result = await googleAuth(authResult["code"]);
+        const { name, email, image } = result.data.user;
+        console.log("result.data.user", result.data.user);
+      }
     } catch (error) {
       console.log("Error while requesting google code", error);
     }
