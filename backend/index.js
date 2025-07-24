330cf3dd04adf51090de5ce1fb7ae06f482ad66b
charu <charupsharma5@gmail.com>
2025-07-25 00:42:13 +0530

    created index and util file

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+require("./models/dbConnect")
+const authRoutes = require('./routes/authRouter');
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+console.log('typeof authRoutes:', typeof authRoutes);
+
+app.use('/auth', authRoutes);
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        message: `Can't find ${req.originalUrl} on the server`,
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
