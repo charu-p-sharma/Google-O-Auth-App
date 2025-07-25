db7dd6620519a4086e392c6e5d67a6606ef346de
charu <charupsharma5@gmail.com>
2025-07-26 00:39:25 +0530

    cors policy updated

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ require("./models/dbConnect")
 const authRoutes = require('./routes/authRouter');
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 
 console.log('typeof authRoutes:', typeof authRoutes);
 
